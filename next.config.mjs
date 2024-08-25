b919b0347162b2383c74119ca25eb86a0551b416
21IZ <wallcroftsempringham@gmail.com>
2024-08-26 00:09:24 +0200

    arreglos16

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
+// next.config.js
+module.exports = {
+    images: {
+      domains: ['2ncxafgeihzol2jw.public.blob.vercel-storage.com'],
+    },
+  }
+  
+
 export default nextConfig;
