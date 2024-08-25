8951c4f991c7fb5fae0b99242b73768840680aed
21IZ <wallcroftsempringham@gmail.com>
2024-08-25 23:12:47 +0200

    arreglos9

--- src/app/components/Card.js
+++ src/app/components/Card.js
@@ -5,42 +5,41 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { useMenuContext } from '../context/MenuContext';
 
-const Card = ({ imagen, nombre, precio, descripcion, id }) => {
+const Card = ({ menuItems }) => {
   const { setSelectedItem } = useMenuContext();
 
-  const handleClick = () => {
-    setSelectedItem({ imagen, nombre, precio, descripcion, id });
+  const handleClick = (item) => {
+    setSelectedItem(item);
   };
 
-  // Check if the item is valid before rendering
-  if (!nombre || !precio) {
-    return null; // Don't render invalid items
-  }
-
   return (
-    <Link href={`/menuDetail/${encodeURIComponent(id)}`} onClick={handleClick} className="card-link">
-      <div className="card">
-        {imagen ? (
-          <Image 
-            src={imagen} 
-            alt={nombre} 
-            width={200}
-            height={130}
-            className="card-image"
-            onError={(e) => {
-              e.target.onerror = null;
-              e.target.src = "/placeholder.jpg"; // Asegúrate de tener esta imagen en tu carpeta public
-            }}
-          />
-        ) : (
-          <div className="card-image-placeholder">No image available</div>
-        )}
-        <div className="card-content">
-          <h2 className="card-title">{nombre}</h2>
-          <p className="card-price">${precio}</p>
-        </div>
-      </div>
-    </Link>
+    <>
+      {menuItems.map((item) => (
+        <Link href={`/menuDetail/${encodeURIComponent(item.id)}`} key={item.id} onClick={() => handleClick(item)} className="card-link">
+          <div className="card">
+            {item.imagen ? (
+              <Image 
+                src={item.imagen} 
+                alt={item.nombre} 
+                width={200}
+                height={130}
+                className="card-image"
+                onError={(e) => {
+                  e.target.onerror = null;
+                  e.target.src = "/placeholder.jpg";
+                }}
+              />
+            ) : (
+              <div className="card-image-placeholder">No image available</div>
+            )}
+            <div className="card-content">
+              <h2 className="card-title">{item.nombre}</h2>
+              <p className="card-price">${item.precio}</p>
+            </div>
+          </div>
+        </Link>
+      ))}
+    </>
   );
 };
 
